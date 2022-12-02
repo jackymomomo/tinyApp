eba9f72b86f0ff8c4ef86964fb5d6c968ff2895f
jackymomomo <jacklloydrobinson@gmail.com>
2022-12-02 03:33:44 -0800

    Add GET route to expressserver.js and render using accompanying template Add POST route to expressserver.js to receive form submission

--- express_server.js
+++ express_server.js
@@ -4,21 +4,34 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let randomString = ''
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789'
+  for (let x = 0; x < 7; x++){
+    randomString = Math.floor(Math.random() * char.length)
+  }
+  return randomString
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new')
+  })
+app.post("/urls", (req, res) => {
+  console.log(req.body)
+  res.send('OK')
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render("urls_show", templateVars);
 });
-
-app.get("/urls/new", (req, res) => {
-res.render('urls_new', templateVars)
-})
-
 
 app.get("/", (req, res) => {
   res.redirect('/urls')
@@ -32,9 +45,6 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templatevars )
 })
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body> HELLO <b>WORLDS</b></body></html>\n')
-})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
